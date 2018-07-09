95331902fd7ab39a49cc352f1d5ba097b10359b4
Jeroen van Beuningen <40868734+JvBtje@users.noreply.github.com>
2018-07-09 18:37:27 +0200

    Add files via upload

---
+++ 500.php
@@ -0,0 +1,16 @@
+<?php
+// Init session settings
+include("DB.php");
+$link = mysqli_connect($DbServer, $DbUsername, $DbPassword,$Db);
+include("./system/include.php");
+session_start();
+$query = "SELECT RedirectLogin, RedirectIndex, Redirect404, Redirect400, Redirect401, Redirect403, Redirect500 FROM system";
+$result = mysqli_query($link,$query);
+if (!$result) {
+	die('Query failed: ' . mysqli_error($link));
+}
+while($row = mysqli_fetch_array($result)){
+	header( 'Location: '.$row["Redirect500"] ) ; 
+	exit;
+}
+?>
